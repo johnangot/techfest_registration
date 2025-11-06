0f244e816328fa9cf1eefb5e9f100ba409019723
John Angot <johnchristopher.angot@benilde.edu.ph>
2025-11-06 12:27:34 +0800

    Implemented participant registration (Task 1)

---
+++ techfest_registration.py
@@ -0,0 +1,11 @@
+﻿print("Welcome to SMIT TechFest!")
+print("Event organized by John Angot of APPDAET BTCS0")
+
+try:
+    n = int(input("How many participants will register? ").strip())
+except:
+    n = 0
+
+if n <= 0:
+    print("Invalid number of participants.")
+    raise SystemExit
